d891b082647ac5ded6e0fc194b523b2730527c77
VizKey01 <micmin0101@gmail.com>
2024-01-11 09:56:46 +0700

    error change

--- src/components/animation/animation.tsx
+++ src/components/animation/animation.tsx
@@ -1,8 +1,8 @@
 import classNames from 'classnames';
 import styles from './animation.module.scss';
-import Lottie from 'lottie-react'
-import man from '../../assets/Man-animation.json'
-import {motion} from 'framer-motion'
+// import Lottie from 'lottie-react'
+// import man from '../../assets/Man-animation.json'
+// import {delay, motion} from 'framer-motion'
 
 export interface AnimationProps {
     className?: string;
@@ -13,5 +13,14 @@ export interface AnimationProps {
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const Animation = ({ className }: AnimationProps) => {
-    return <div className={classNames(styles.root, className)}>Animation</div>;
+    return <div className={classNames(styles.root, className)}>
+        {/* <motion.div 
+            intitial={{opacity: 0, scale : 0}} 
+            animate={{opacity: 1, scale: 1}}
+            transition={{ delay: 0.6, duration: 0.5}}
+        >
+            <Lottie animationData={man} />
+        </motion.div> */}
+        animation
+    </div>;
 };
