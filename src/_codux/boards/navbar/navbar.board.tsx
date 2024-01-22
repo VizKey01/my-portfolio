28e1c0f2a1ed3c74128800faffe7f9e50c38dbe4
VizKey01 <micmin0101@gmail.com>
2024-01-22 15:11:22 +0700

    test screen

--- src/_codux/boards/navbar/navbar.board.tsx
+++ src/_codux/boards/navbar/navbar.board.tsx
@@ -6,6 +6,7 @@ export default createBoard({
     Board: () => <Navbar />,
     isSnippet: true,
     environmentProps: {
-        canvasHeight: 5
+        canvasHeight: 5,
+        canvasWidth: 851
     }
 });
